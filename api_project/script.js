9fce15dfb1fca5f3556c1df1231794de63b459a8
Yeony Choi <yeony612@gmail.com>
2022-06-29 19:43:49 -0400

    fixed all js problems

--- api_project/script.js
+++ api_project/script.js
@@ -1,29 +1,32 @@
 console.log("js is working!");
 
 const getFacts = async () => {
-  const catFacts = await axios.get("https://cat-fact.herokuapp.com/facts");
+  const catFacts = await axios.get("https://catfact.ninja/fact?max_length=140");
   console.log(catFacts);
 };
 
 getFacts();
 
 const catButton = document.querySelector(".cat-fact-button");
-const resultDiv = document.querySelector(".cat-fact");
+const factDiv = document.querySelector(".cat-fact");
 const imageDiv = document.querySelector(".cat-image");
 
-// const generateFact = async () => {
-//   console.log("generate fact function working!");
-//   let response = await axios.get(`https://cat-fact.herokuapp.com/facts/random`);
-//   let data = response.data.text;
-//   resultDiv.innerHTML = `<p>${data}</p>`;
-// };
+const generateFact = async () => {
+  console.log("generate fact function working!");
+  let responseFact = await axios.get(
+    `https://catfact.ninja/fact?max_length=140`
+  );
+  let fact = responseFact.data.fact;
+  console.log(fact);
+  factDiv.innerHTML = `<p>${fact}</p>`;
+};
+
+generateFact();
 
 const generateImage = async () => {
   console.log("generate Image function working!");
   let response = await axios.get(`https://api.thecatapi.com/v1/images/search`);
-  console.log(response);
   let image = response.data[0].url;
-  console.log(image);
   imageDiv.innerHTML = `<img src="${image}" width="600" height="100%"></img>`;
 };
 
