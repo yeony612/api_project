acae4e15dd311fe9fee3c0b95621ec3ebb6a0d64
Yeony Choi <yeony612@gmail.com>
2022-06-29 19:12:17 -0400

    trouble shooting

--- api_project/script.js
+++ api_project/script.js
@@ -1,12 +1,7 @@
 console.log("js is working!");
 
 const getFacts = async () => {
-  const catFacts = await axios.get("https://cat-fact.herokuapp.com/facts", {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-  });
+  const catFacts = await axios.get("https://cat-fact.herokuapp.com/facts");
   console.log(catFacts);
 };
 
@@ -14,12 +9,22 @@ getFacts();
 
 const catButton = document.querySelector(".cat-fact-button");
 const resultDiv = document.querySelector(".cat-fact");
+const imageDiv = document.querySelector(".cat-image");
 
-const generateFact = async () => {
-  console.log("generate fact function working!");
-  let response = await axios.get(`https://cat-fact.herokuapp.com/facts/random`);
-  let data = response.data.text;
-  resultDiv.innerHTML = `<p>${data}</p>`;
+// const generateFact = async () => {
+//   console.log("generate fact function working!");
+//   let response = await axios.get(`https://cat-fact.herokuapp.com/facts/random`);
+//   let data = response.data.text;
+//   resultDiv.innerHTML = `<p>${data}</p>`;
+// };
+
+const generateImage = async () => {
+  console.log("generate Image function working!");
+  let response = await axios.get(`https://api.thecatapi.com/v1/images/search`);
+  console.log(response);
+  let image = response.data[0].url;
+  console.log(image);
+  imageDiv.innerHTML = `<img src="${image}" width="600" height="100%"></img>`;
 };
 
-catButton.addEventListener("click", generateFact);
+generateImage();
