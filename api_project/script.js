e666eed3b6e78a078ecabe1cd56406f5552c0e27
Yeony Choi <yeony612@gmail.com>
2022-06-29 14:33:47 -0400

    pushing

--- api_project/script.js
+++ api_project/script.js
@@ -1,7 +1,9 @@
 console.log("js is working!");
 
 const getFacts = async () => {
-  const catFacts = await axios.get("https://cat-fact.herokuapp.com/facts");
+  const catFacts = await axios.get("https://cat-fact.herokuapp.com/facts", {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  });
   console.log(catFacts);
 };
 
@@ -12,9 +14,9 @@ const resultDiv = document.querySelector(".cat-fact");
 
 const generateFact = async () => {
   console.log("generate fact function working!");
-  let response = await axios.get(`https://cat-fact.herokuapp.com/facts/random`);
-  let data = response.data.text;
-  resultDiv.innerHTML = `<p>${data}</p>`;
+  // let response = await axios.get(`https://cat-fact.herokuapp.com/facts/random`);
+  // let data = response.data.text;
+  // resultDiv.innerHTML = `<p>${data}</p>`;
 };
 
 catButton.addEventListener("click", generateFact);
