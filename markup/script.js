9a027e661149c0770b360bece6b1b7b0c61ee319
yeony612 <77176550+yeony612@users.noreply.github.com>
2022-08-18 17:49:37 -0400

    updated website

--- markup/script.js
+++
@@ -1,9 +0,0 @@
-console.log("js is working!");
-
-const apiFunction = () => {};
-
-const catButton = document.querySelector(".cat-fact-button");
-const catFact = () => {
-  console.log("button function working!");
-};
-catButton.addEventListener("click", catFact);
